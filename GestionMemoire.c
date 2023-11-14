704e2e99ea4426d659338e3dba4f78b10748370f
Leonard Pouliot <leonard.pouliot@polymtl.com>
2023-11-14 16:58:49 -0500

    rechercherTLB

--- GestionMemoire.c
+++ GestionMemoire.c
@@ -1,44 +1,68 @@
 #include "./libs/lib.h"
+#include <stdio.h>
+
 unsigned long TAILLE_PAGE = 1024;
 
-unsigned int calculerNumeroDePage(unsigned long adresse) {
-    return adresse / TAILLE_PAGE;
+unsigned int calculerNumeroDePage(unsigned long adresse)
+{
+	return adresse / TAILLE_PAGE;
 }
 
-unsigned long calculerDeplacementDansLaPage(unsigned long adresse) {
-     return adresse % TAILLE_PAGE;
+unsigned long calculerDeplacementDansLaPage(unsigned long adresse)
+{
+	return adresse % TAILLE_PAGE;
 }
 
-unsigned long calculerAdresseComplete(unsigned int numeroDePage, unsigned long deplacementDansLaPage) {
-     return (numeroDePage*TAILLE_PAGE) + deplacementDansLaPage;
+unsigned long calculerAdresseComplete(unsigned int numeroDePage, unsigned long deplacementDansLaPage)
+{
+	return (numeroDePage * TAILLE_PAGE) + deplacementDansLaPage;
 }
 
-void rechercherTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
+void rechercherTLB(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
+	unsigned int numPage = calculerNumeroDePage(req->adresseVirtuelle);
+	unsigned long depPage = calculerDeplacementDansLaPage(req->adresseVirtuelle);
+	for (int i = 0; i < TAILLE_TLB; i++)
+	{
+		if (mem->tlb->numeroPage[i] == numPage && mem->tlb->entreeValide[i])
+		{
+			unsigned int numCadre = mem->tlb->numeroCadre[i];
+			req->adressePhysique = calculerAdresseComplete(numCadre, depPage);
+			req->estDansTLB = 1;
+			mem->tlb->dernierAcces[i] = req->date;
+
+			return;
+		}
+	}
+	req->adressePhysique = 0;
+	req->estDansTLB = 0;
+}
+
+void rechercherTableDesPages(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
 	// TODO
 }
 
-void rechercherTableDesPages(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
+void ajouterDansMemoire(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
 	// TODO
 }
 
-void ajouterDansMemoire(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-	//TODO
-}
-
-void mettreAJourTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
+void mettreAJourTLB(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
 	// TODO
 }
 
 // NE PAS MODIFIER
-int main() {
-    evaluate(
-		&calculerNumeroDePage, 
-		&calculerDeplacementDansLaPage, 
-		&calculerAdresseComplete, 
-        &rechercherTLB, 
+int main()
+{
+	evaluate(
+		&calculerNumeroDePage,
+		&calculerDeplacementDansLaPage,
+		&calculerAdresseComplete,
+		&rechercherTLB,
 		&rechercherTableDesPages,
 		&mettreAJourTLB,
-		&ajouterDansMemoire
-    );
-    return 0;
+		&ajouterDansMemoire);
+	return 0;
 }
