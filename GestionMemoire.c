42e55f03909bd01a6619b56d2f9562b40e80c1c5
Leonard Pouliot <leonard.pouliot@polymtl.com>
2023-11-15 16:02:23 -0500

    last push

--- GestionMemoire.c
+++ GestionMemoire.c
@@ -31,6 +31,7 @@ void rechercherTLB(struct RequeteMemoire *req, struct SystemeMemoire *mem)
 {
 	unsigned int numPage = calculerNumeroDePage(req->adresseVirtuelle);
 	unsigned long depPage = calculerDeplacementDansLaPage(req->adresseVirtuelle);
+
 	for (int i = 0; i < TAILLE_TLB; i++)
 	{
 		if (mem->tlb->numeroPage[i] == numPage && mem->tlb->entreeValide[i])
@@ -38,18 +39,20 @@ void rechercherTLB(struct RequeteMemoire *req, struct SystemeMemoire *mem)
 			unsigned int numCadre = mem->tlb->numeroCadre[i];
 			req->adressePhysique = calculerAdresseComplete(numCadre, depPage);
 			req->estDansTLB = 1;
-			
+
 			time(&req->date);
 			mem->tlb->dernierAcces[i] = req->date;
 
 			return;
 		}
 	}
+
 	req->adressePhysique = 0;
 	req->estDansTLB = 0;
 }
 
-void rechercherTableDesPages(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
+void rechercherTableDesPages(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
 	unsigned int numeroDePage = calculerNumeroDePage(req->adresseVirtuelle);
 	unsigned long offsetAdresse = calculerDeplacementDansLaPage(req->adresseVirtuelle);
 
@@ -66,32 +69,55 @@ void rechercherTableDesPages(struct RequeteMemoire* req, struct SystemeMemoire* 
 	}
 }
 
-void ajouterDansMemoire(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
+void ajouterDansMemoire(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
+	unsigned int numeroDePage = calculerNumeroDePage(req->adresseVirtuelle);
+	unsigned long offsetAdresse = calculerDeplacementDansLaPage(req->adresseVirtuelle);
+
+	for (unsigned int i = 0; i < TAILLE_MEMOIRE; i++)
+	{
+		if (!mem->memoire->utilisee[i])
+		{
+			req->adressePhysique = calculerAdresseComplete(i, offsetAdresse);
+
+			mem->memoire->numeroPage[i] = numeroDePage;
+			mem->memoire->dateCreation[i] = req->date;
+			mem->memoire->dernierAcces[i] = req->date;
+			mem->memoire->utilisee[i] = 1;
+
+			return;
+		}
+	}
 }
 
-
-void mettreAJourTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-    int indiceRemplacement = 0;
+void mettreAJourTLB(struct RequeteMemoire *req, struct SystemeMemoire *mem)
+{
+	int indiceRemplacement = 0;
 	int numeroPage = calculerNumeroDePage(req->adresseVirtuelle);
 	int numeroCadre = calculerNumeroDePage(req->adressePhysique);
 	unsigned long plusAncienneDate = mem->tlb->dateCreation[numeroPage];
-	for (int i = 0; i < TAILLE_TLB; i++) {
-		if(mem->tlb->entreeValide[i]){
-			if (mem->tlb->dateCreation[i] < plusAncienneDate) {
+
+	for (int i = 0; i < TAILLE_TLB; i++)
+	{
+		if (mem->tlb->entreeValide[i])
+		{
+			if (mem->tlb->dateCreation[i] < plusAncienneDate)
+			{
 				plusAncienneDate = mem->tlb->dateCreation[i];
 				indiceRemplacement = i;
 			}
 		}
-		else{
+		else
+		{
 			indiceRemplacement = i;
 			break;
 		}
 	}
-    mem->tlb->numeroPage[indiceRemplacement] = numeroPage;
-    mem->tlb->numeroCadre[indiceRemplacement] = numeroCadre;
-    mem->tlb->entreeValide[indiceRemplacement] = 1;
-    mem->tlb->dernierAcces[indiceRemplacement] = req->date;
-    mem->tlb->dateCreation[indiceRemplacement] = req->date;
+	mem->tlb->numeroPage[indiceRemplacement] = numeroPage;
+	mem->tlb->numeroCadre[indiceRemplacement] = numeroCadre;
+	mem->tlb->entreeValide[indiceRemplacement] = 1;
+	mem->tlb->dernierAcces[indiceRemplacement] = req->date;
+	mem->tlb->dateCreation[indiceRemplacement] = req->date;
 }
 
 // NE PAS MODIFIER
